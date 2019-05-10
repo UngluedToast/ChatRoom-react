4afbd3f691ff2e00964d946b9f068c852c73c05b
victor truong <2truong93@gmail.com>
2019-05-10 15:09:35 -0400

    user form that sets user state

--- src/Lists/ChatList.js
+++ src/Lists/ChatList.js
@@ -4,7 +4,6 @@ import React from 'react'
 
 
 function ChatList({messages}) {
-    console.log(`look at me `, messages)
     const messageItems = messages.map((m, i) => (
     <li key={i}>
         {m}
